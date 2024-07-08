5070be24d9010ddf506e21a2870b55f433240f86
Bruno Gonçalves <bigbruno@gmail.com>
2024-07-07 21:37:21 -0300

    Add option to auto switch for bluetooth headphones

--- usr/share/bigbashview/apps/microphone/actions.sh
+++ usr/share/bigbashview/apps/microphone/actions.sh
@@ -4,6 +4,8 @@ if [[ "$1" = "start" ]]; then
     systemctl --user start noise-reduction-pipewire
 elif [[ "$1" = "stop" ]]; then
     systemctl --user stop noise-reduction-pipewire
-elif [[ "$1" = "status" ]]; then
-    pipewire-noise-remove status
+else
+    pipewire-noise-remove "$1"
 fi
+
+# See more options in pipewire-noise-remove --help
