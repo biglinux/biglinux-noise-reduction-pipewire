9875129d9f9cc1e164c2e6e5df5279dfccf36d84
Bruno Gonçalves <bigbruno@gmail.com>
2026-06-18 14:29:09 -0300

    build: remove vendored PipeWire bindings

--- vendor/libspa-0.10.0/src/constants.rs
+++
@@ -1,7 +0,0 @@
-// Copyright The pipewire-rs Contributors.
-// SPDX-License-Identifier: MIT
-
-//! libspa constants.
-
-/// Invalid ID intended for errors or as a sentinel.
-pub const ID_INVALID: u32 = u32::MAX;
--- vendor/pipewire-0.10.0/src/utils.rs
+++
@@ -1,8 +0,0 @@
-// Copyright The pipewire-rs Contributors.
-// SPDX-License-Identifier: MIT
-
-use std::thread;
-
-pub fn assert_main_thread() {
-    assert_eq!(thread::current().name(), Some("main"));
-}
